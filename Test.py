4c4080504c61ca155c79e4f0818110d752105856
Demiana404 <demianaashraf404@gmail.com>
2025-12-10 00:10:03 +0200

    Add pygame image loading and display test

--- Test.py
+++ Test.py
@@ -1,1 +1,20 @@
-print("Hello, worldd!")
+from os.path import join
+from pygame.image import load
+
+# The path to the event sound file.
+event_sound = join("sounds", "event.ogg")
+
+# The path to the first disc drop sound file.
+disc_drop_1 = join("sounds", "disc_drop_1.wav")
+
+# The path to the second disc drop sound file.
+disc_drop_2 = join("sounds", "disc_drop_2.wav")
+
+# Load the red coin image from the images folder.
+red_coin = load(join("Images", "redball90px.png"))
+
+# Load the yellow coin image from the images folder.
+yellow_coin = load(join("Images", "yellowball90px.png"))
+
+# Load the black coin image from the images folder.
+black_coin = load(join("Images", "blackball91px.png"))
